1752e0308b9a31380863e6949cd71c78dfd60e33
Yakov <yakov.shev@gmail.com>
2024-10-11 17:39:27 +0200

    Added gameboard

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleRegisterTurn(rowIndex, colIndex, playerSymbol) {
+    setGameBoard((prevTurns) => {
+      let newTurns = prevTurns;
+      newTurns[rowIndex][colIndex] = playerSymbol;
+      return newTurns;
+    });
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => {
+        return (
+          <li key={rowIndex}>
+            <ol>
+              {row.map((playerSymbol, colIndex) => {
+                return (
+                  <li key={colIndex}>
+                    <button
+                      onClick={() =>
+                        handleRegisterTurn(rowIndex, colIndex, "X")
+                      }
+                    >
+                      {playerSymbol}
+                    </button>
+                  </li>
+                );
+              })}
+            </ol>
+          </li>
+        );
+      })}
+    </ol>
+  );
+}
