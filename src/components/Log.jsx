2338f0186da585a1eab85cff0bf15af41e14f450
Yakov <yakov.shev@gmail.com>
2024-10-13 01:52:04 +0200

    Changed key value of item in logs

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,8 +1,10 @@
 export default function Log({ gameTurns }) {
   return (
     <ol id="log">
-      {gameTurns.map((turn, turnIndex) => (
-        <li key={turnIndex}>{`${turn.player} placed [${turn.square.row + 1}, ${turn.square.col + 1}]`}</li>
+      {gameTurns.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>{`${
+          turn.player
+        } placed [${turn.square.row + 1}, ${turn.square.col + 1}]`}</li>
       ))}
     </ol>
   );
